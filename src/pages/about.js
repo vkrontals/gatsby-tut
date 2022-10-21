314c74573b1ce33bc7794c6932eecd7a1eb4078f
Valters Krontals <valters.krontals@esl-asia.com>
2022-10-21 15:18:32 +0200

    first task complete

---
+++ src/pages/about.js
@@ -0,0 +1,16 @@
+import * as React from 'react'
+import Layout from '../components/layout'
+
+const AboutPage = () => {
+	return (
+		<Layout pageTitle="About page">
+			<p> This is the about page</p>
+		</Layout>
+	)
+}
+export const Head = () => (
+ <>
+   <title>About me</title> 
+  </>
+)
+export default AboutPage
